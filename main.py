9bca128b02045a4904c95e1a5de5a45ba1b3cf37
Jacob E <esealsuperior@gmail.com>
2024-09-03 10:43:16 +0200

    klar med upg 1,2 och 3

--- main.py
+++ main.py
@@ -23,9 +23,40 @@ def read_from_file(path: str):
     with open(path, "r" ,encoding="utf-8") as f:
         return f.readlines()
 
+def count_words():
+    sentences = ''.join(read_from_file("en_resa_genom_svenska_skogen.txt"))
+    sentences.replace('\n',' ')
+    sentences=sentences.split()
+    word_count=len(sentences) 
+    print(word_count)
+
+def most_frequent():
+    sentences = ''.join(read_from_file("en_resa_genom_svenska_skogen.txt"))
+    sentences.replace('\n',' ')
+    sentences=sentences.lower()
+    sentences=sentences.split()
+    dict={}
+    for word in sentences:
+        if word in dict:
+            dict[word]+=1
+        else:
+            dict[word]=1
+    most_freq=max(dict,key=dict.get)
+    print(most_freq)
+
+def avg_len():
+    sentences = ''.join(read_from_file("en_resa_genom_svenska_skogen.txt"))
+    sentences=sentences.replace('\n',' ')
+    sentences=sentences.lower()
+    sentences2=sentences
+    sentences=sentences.replace(' ','')    
+    sentences2=sentences2.split()
+    avg=len(sentences)/len(sentences2)
+    print(avg)
+
 def main():
     
     sentences = read_from_file("en_resa_genom_svenska_skogen.txt") # Här har du nu en lista av strängar från den inlästa filen.
 
 if __name__ == "__main__":
-    main()
+    avg_len()
